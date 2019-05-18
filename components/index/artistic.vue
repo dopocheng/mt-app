b1c027f065f291993fd261d0faae76489a689cd6
Pitcher <747015697@qq.com>
2019-05-18 22:40:26 +0800

    美团APP首页初次提交
    1.设计划分页面布局
    2.页面样式
    3.下步 掉接口与后台交互

---
+++ components/index/artistic.vue
@@ -0,0 +1,103 @@
+<template>
+    <section class="m-istyle">
+        <dl >
+            <dt>有格调</dt>
+            <dd
+                :class="{active:kind==='all'}"
+                kind="all"
+                keyword="景点">全部</dd>
+            <dd
+                :class="{active:kind==='part'}"
+                kind="part"
+                keyword="美食">约会聚餐</dd>
+            <dd
+                :class="{active:kind==='spa'}"
+                kind="spa"
+                keyword="丽人">丽人SPA</dd>
+            <dd
+                :class="{active:kind='movie'}"
+                kind="movie"
+                keyword="电影">电影演出</dd>
+            <dd
+                :class="{active:kind='travel'}"
+                kind="travel"
+                keyword="旅游">品质出游</dd>
+        </dl>
+
+        <ul class="ibody">
+            <li
+                v-for="item in cur" :key="item.title">
+                <el-card
+                    :body-style="{ padding: '0px' }"
+                    shadow="never">
+                    <img 
+                        :src="item.img" 
+                        class="image">
+                    <ul class="cbody">
+                        <li class="title">{{ item.title }}</li>
+                        <li class="pos">{{ item.pos }}</li>
+                        <li class="price">￥<em>{{ item.price }}</em><span>/起</span></li>
+                    </ul>
+                </el-card>
+            </li>
+        </ul>
+    </section>
+</template>
+
+<script>
+export default {
+    data: () => {
+        return {
+            kind: 'all',
+            list: {
+                all: [{
+                    title: '吉山尚合酒店',
+                    pos: '酒店周边',
+                    price: '530',
+                    img: '//p1.meituan.net/tdchotel/836bb3e109b1c705d2e5f50d9bff70dc9820182.jpg@368w_208h_1e_1c',
+                    url: '//abc.com'
+                }, {
+                    title: '耀莱国际影城',
+                    pos: '免押金，可停车',
+                    price: Math.ceil(Math.random() * 400),
+                    img: '//p1.meituan.net/tdchotel/836bb3e109b1c705d2e5f50d9bff70dc9820182.jpg@368w_208h_1e_1c',
+                    url: '//abc.com'
+                }, {
+                    title: '三只耳河鲜馆',
+                    pos: '双人套餐',
+                    price: '520',
+                    img: '//p1.meituan.net/tdchotel/836bb3e109b1c705d2e5f50d9bff70dc9820182.jpg@368w_208h_1e_1c',
+                    url: '//abc.com'
+                }, {
+                    title: '良子健身',
+                    pos: '良子健身',
+                    price: '510',
+                    img: '//p1.meituan.net/tdchotel/836bb3e109b1c705d2e5f50d9bff70dc9820182.jpg@368w_208h_1e_1c',
+                    url: '//abc.com'
+                }],
+                part: [],
+                spa: [],
+                movie: [],
+                travel: []
+            }
+        }
+    },
+
+    computed: {
+    cur: function () {
+      return this.list['all']
+    }
+  },
+
+  methods: {
+      over: function() {
+
+      }
+  }
+}
+</script>
+
+<style lang="scss">
+    @import '@/assets/css/index/artistic.scss';
+</style>
+
---
+++ components/public/header/nav.vue
@@ -0,0 +1,111 @@
+<template>
+    <div class="m-nav">
+        <ul class="nav">
+            <li class="list">
+                <nuxt-link to="/my">我的美团</nuxt-link>
+                <dl>
+                    <dd><nuxt-link to="/order">我的订单</nuxt-link></dd>
+                    <dd><nuxt-link to="/order">我的收藏</nuxt-link></dd>
+                    <dd><nuxt-link to="/order">抵用券</nuxt-link></dd>
+                    <dd><nuxt-link to="/order">账户管理</nuxt-link></dd>
+                </dl>
+            </li>
+            <li>
+                <nuxt-link to="/order">手机app</nuxt-link>
+            </li>
+            <li class="list bd">
+                <nuxt-link to="/center">商家中心</nuxt-link>
+            </li>
+            <li class="list site">
+                <nuxt-link to="/site">网站导航</nuxt-link>
+                <div class="subContainer">
+                    <dl class="hotel">
+                        <dt>酒店旅欧</dt>
+                        <dd>吉山酒店</dd>
+                        <dd>吉山酒店</dd>
+                        <dd>吉山酒店</dd>
+                        <dd>吉山酒店</dd>
+                        <dd>吉山酒店</dd>
+                        <dd>吉山酒店</dd>
+                        <dd>吉山酒店</dd>
+                        <dd>吉山酒店</dd>
+                        <dd>吉山酒店</dd>
+                    </dl>
+                    <dl class="food">
+                        <dt>吃美食</dt>
+                        <dd>烧烤</dd>
+                        <dd>烧烤</dd>
+                        <dd>烧烤</dd>
+                        <dd>烧烤</dd>
+                        <dd>烧烤</dd>
+                        <dd>烧烤</dd>
+                        <dd>烧烤</dd>
+                        <dd>烧烤</dd>
+                        <dd>烧烤</dd>
+                    </dl>
+                    <dl class="movie">
+                        <dt>看电影</dt>
+                        <dd>驯龙高手</dd>
+                        <dd>驯龙高手</dd>
+                        <dd>驯龙高手</dd>
+                        <dd>驯龙高手</dd>
+                        <dd>驯龙高手</dd>
+                        <dd>驯龙高手</dd>
+                        <dd>驯龙高手</dd>
+                        <dd>驯龙高手</dd>
+                    </dl>
+                    <dl class="app">
+                        <dt>手机应用</dt>
+                        <dd>
+                            <a href="#">
+                                <img
+                                    src="//s0.meituan.net/bs/fe-web-meituan/e5eeaef/img/appicons/meituan.png"
+                                    title="美团app"
+                                    alt="美团app">
+                            </a>
+                        </dd>
+                        <dd>
+                            <a href="#">
+                                <img
+                                    src="//s1.meituan.net/bs/fe-web-meituan/404d350/img/appicons/waimai.png"
+                                    title="外卖app"
+                                    alt="外卖app">
+                            </a>
+                        </dd>
+                        <dd>
+                            <a href="#">
+                                <img
+                                    src="//s0.meituan.net/bs/fe-web-meituan/404d350/img/appicons/zhenguo.png"
+                                    title="榛果app"
+                                    alt="榛果app">
+                            </a>
+                        </dd>
+                        <dd>
+                            <a href="#">
+                                <img
+                                    src="//s1.meituan.net/bs/fe-web-meituan/404d350/img/appicons/dianping.png"
+                                    title="点评app"
+                                    alt="点评app">
+                            </a>
+                        </dd>
+                        <dd>
+                            <a href="#">
+                                <img
+                                    src="//s1.meituan.net/bs/fe-web-meituan/404d350/img/appicons/maoyan.png"
+                                    title="猫眼app"
+                                    alt="猫眼app">
+                            </a>
+                        </dd>
+                    </dl>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    
+} 
+</script>
+<style lang="css"></style>
